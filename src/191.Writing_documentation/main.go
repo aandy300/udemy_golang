1ed18b57db83ba026a6826d45a9852e17400b8c9
aandy300 <aandy300@gmail.com>
2020-08-28 09:37:30 +0800

    fix_20200828

---
+++ src/191.Writing_documentation/main.go
@@ -0,0 +1,12 @@
+// 190. Writing documentation
+package main
+
+import (
+	"fmt"
+	"github.com/aandy300/udemy_golang/tree/master/src/191.Writing%20documentation/01/mymathTwo"
+)
+
+func main() {
+	fmt.Println("1")
+	mymathTwo.Sum(1)
+}
